876dde698c7af0def215c3b7faee1a556cdae8ad
M.W.S.S. Vijayantha <ssvsenith@gmail.com>
2025-10-06 16:46:29 +0530

    feat: add chat header
    - update assets folder

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -2,18 +2,105 @@ import icon from "./icon.svg";
 import user1 from "./user-1.jpeg";
 import user2 from "./user-2.jpeg";
 import user3 from "./user-3.jpg";
+import img1 from "./chocolate.jpg";
+import img2 from "./coffee.jpg";
 import avatarIcon from "./avatar-icon.svg";
 
 const dummyUserData = [
-  { profilePic: user1, fullName: "Clara Mitchell", _id: 3242 },
-  { profilePic: user2, fullName: "Ava Kim", _id: 3243 },
-  { profilePic: user3, fullName: "Ryan Patel", _id: 3244 },
-  { profilePic: user1, fullName: "Clara Mitchell", _id: 3248 },
-  { profilePic: user2, fullName: "Ava Kim", _id: 3249 },
-  { profilePic: user3, fullName: "Ryan Patel", _id: 3250 },
-  { fullName: "Maya Ross", _id: 3245 },
-  { fullName: "Oliver Grant", _id: 3246 },
-  { fullName: "Emilia Brooks", _id: 3247 },
+  {
+    _id: "680f50aaf10f3cd28382ecf2",
+    email: "test1@greatstack.dev",
+    fullName: "Alison Martin",
+    profilePic: user1,
+    bio: "Hi Everyone, I am Using QuickChat",
+  },
+  {
+    _id: "680f50e4f10f3cd28382ecf9",
+    email: "test2@greatstack.dev",
+    fullName: "Martin Johnson",
+    profilePic: user2,
+    bio: "Hi Everyone, I am Using QuickChat",
+  },
+  {
+    _id: "680f510af10f3cd28382ed01",
+    email: "test3@greatstack.dev",
+    fullName: "Enrique Martinez",
+    profilePic: user3,
+    bio: "Hi Everyone, I am Using QuickChat",
+  },
+  {
+    _id: "680f5137f10f3cd28382ed10",
+    email: "test4@greatstack.dev",
+    fullName: "Marco Jones",
+    profilePic: user1,
+    bio: "Hi Everyone, I am Using QuickChat",
+  },
+  {
+    _id: "680f516cf10f3cd28382ed11",
+    email: "test5@greatstack.dev",
+    fullName: "Richard Smith",
+    profilePic: user3,
+    bio: "Hi Everyone, I am Using QuickChat",
+  },
 ];
 
-export { icon, avatarIcon, dummyUserData };
+const dummyMessageData = [
+  {
+    _id: "680f571ff10f3cd28382f094",
+    senderId: "680f5116f10f3cd28382ed02",
+    receiverId: "680f50e4f10f3cd28382ecf9",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    seen: true,
+    createdAt: "2025-04-28T10:23:27.844Z",
+  },
+  {
+    _id: "680f5726f10f3cd28382f0b1",
+    senderId: "680f50e4f10f3cd28382ecf9",
+    receiverId: "680f5116f10f3cd28382ed02",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    seen: true,
+    createdAt: "2025-04-28T10:23:34.520Z",
+  },
+  {
+    _id: "680f5729f10f3cd28382f0b6",
+    senderId: "680f5116f10f3cd28382ed02",
+    receiverId: "680f50e4f10f3cd28382ecf9",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    seen: true,
+    createdAt: "2025-04-28T10:23:37.301Z",
+  },
+  {
+    _id: "680f572cf10f3cd28382f0bb",
+    senderId: "680f50e4f10f3cd28382ecf9",
+    receiverId: "680f5116f10f3cd28382ed02",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    seen: true,
+    createdAt: "2025-04-28T10:23:40.334Z",
+  },
+  {
+    _id: "680f573cf10f3cd28382f0c0",
+    senderId: "680f50e4f10f3cd28382ecf9",
+    receiverId: "680f5116f10f3cd28382ed02",
+    image: img1,
+    seen: true,
+    createdAt: "2025-04-28T10:23:56.265Z",
+  },
+  {
+    _id: "680f5745f10f3cd28382f0c5",
+    senderId: "680f5116f10f3cd28382ed02",
+    receiverId: "680f50e4f10f3cd28382ecf9",
+    image: img2,
+    seen: true,
+    createdAt: "2025-04-28T10:24:05.164Z",
+  },
+  {
+    _id: "680f5748f10f3cd28382f0ca",
+    senderId: "680f5116f10f3cd28382ed02",
+    receiverId: "680f50e4f10f3cd28382ecf9",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    seen: true,
+    createdAt: "2025-04-28T10:24:08.523Z",
+  },
+];
+
+export { icon, avatarIcon, dummyUserData, dummyMessageData, user1 };
--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import { MessageSquare } from "lucide-react";
 
-const ChatContainer = ({selectedUser, setSelectedUser}) => {
-  return (
-    <div>ChatContainer</div>
-  )
-}
+import { icon, user1 } from "../assets/assets";
 
-export default ChatContainer
+const ChatContainer = ({ selectedUser, setSelectedUser }) => {
+  return selectedUser ? (
+    <div className="">
+        {/* Header */}
+      <div className="flex items-center gap-3 py-3 mx-4 border-b border-accent-content">
+        <img src={user1} alt="" className="w-8 h-8 object-cover rounded-full" />
+        <p className="flex-1 text-lg text-white flex items-center gap-2 ">
+          Clara Mitchell <div className="w-2 h-2 rounded-full bg-primary"></div>
+        </p>
+      </div>
+
+      {/* Chat area */}
+    </div>
+  ) : (
+    <div className="h-[100vh] border-2 flex flex-col justify-center items-center gap-4">
+      <MessageSquare width={65} height={65} color="gray" />
+      <div className="text-center">
+        <p className="text-xl text-gray-600 font-semibold">Ping Me</p>
+        <p className="text-gray-600">Chat with anyone around the world.</p>
+      </div>
+    </div>
+  );
+};
+
+export default ChatContainer;
