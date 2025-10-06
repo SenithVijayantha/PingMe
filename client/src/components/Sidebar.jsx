cbf2161c062aa91fab76aea86012c7ed327c92c5
M.W.S.S. Vijayantha <ssvsenith@gmail.com>
2025-10-06 10:21:58 +0530

    feat: add a navbar inside the sidebar
    - Change the site icon
    - Create assets.js file and include exports in it

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,12 +1,45 @@
-import React from 'react'
-import { PanelLeftOpen } from 'lucide-react'
+import React from "react";
+import { EllipsisVertical } from "lucide-react";
+import { useNavigate, Link } from "react-router";
+
+import { icon } from "../assets/assets";
 
 const Sidebar = () => {
+  const navigate = useNavigate();
+
   return (
     <div>
-        Sidebar
+      <div className="navbar bg-base-100 shadow-sm">
+        <div className="flex-1">
+            
+          <p className="btn btn-ghost text-lg"><img width={18} src={icon} alt="" />PingMe</p>
+        </div>
+        <div className="flex gap-2">
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <EllipsisVertical />
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <Link to={"/profile"}>Edit Profile</Link>
+              </li>
+              <li>
+                <Link>Logout</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      Sidebar
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
