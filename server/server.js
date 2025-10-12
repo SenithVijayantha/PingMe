c3ce6d45f496fbe70cc492e8a20dd76e5d77eaf3
M.W.S.S. Vijayantha <ssvsenith@gmail.com>
2025-10-12 10:49:15 +0530

    feat: setup socket.io

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import http from "node:http";
+import { Server } from "socket.io";
 
 import { connectDB } from "./config/db.js";
 import userRouter from "./routes/userRoutes.js";
@@ -23,10 +25,40 @@ app.use("/api/status", (req, res) => {
 app.use("/api/auth", userRouter);
 app.use("/api/messages", messageRouter);
 
+// create HTTP server
+const server = http.createServer(app);
+
+// Setup Socket.IO
+export const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+// store online users
+export const userSocketMap = {}; // {userId: socketId}
+
+// Socket.io connection handler
+io.on("connection", (socket) => {
+  const userId = socket.handshake.query.userId;
+  console.log("User connected", userId);
+
+  if (userId) userSocketMap[userId] = socket.id;
+
+  // Emit online users to all connected clients
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", userId);
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  });
+});
+
 // connect DB and start server
 try {
   await connectDB();
-  app.listen(port, () => console.log("Server started on PORT:", port));
+  server.listen(port, () => console.log("Server started on PORT:", port));
 } catch (error) {
   console.error(error);
 }
