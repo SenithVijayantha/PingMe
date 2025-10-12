f398788e64f41f1ddc15d3debfea57b3456f5377
M.W.S.S. Vijayantha <ssvsenith@gmail.com>
2025-10-12 13:55:14 +0530

    fix: rename AuthContext file extension from .js to .jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router";
 
 import "./index.css";
 import App from "./App.jsx";
-import { AuthProvider } from "../context/AuthContext.js";
+import { AuthProvider } from "../context/AuthContext";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
